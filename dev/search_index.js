3abf3b1212e7182a36dcc7be0a68cf8c61af8e83
Documenter.jl <documenter@juliadocs.github.io>
2022-12-26 20:45:00 +0000

    build based on 0df95b8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GreekSyntax.jl","page":"Overview","title":"GreekSyntax.jl","text":"","category":"section"},{"location":"","page":"Overview","title":"Overview","text":"Implementing a simple model of ancient Greek syntax, as presented at https://neelsmith.github.io/GreekSyntax/.","category":"page"},{"location":"","page":"Overview","title":"Overview","text":"read/write simple delimited-text files\nfunctions to compose a variety of  HTML visualizations of syntactically highlighted text\nvisualize syntax graphically with Mermaid diagrams","category":"page"}]
+}
