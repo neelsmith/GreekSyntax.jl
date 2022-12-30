fa7ee9d4f9ce7b6f770b2a985408aa5ad76b5913
Neel Smith <dnsmith.neel@gmail.com>
2022-12-30 04:49:46 -0500

    Better organization of functions

--- src/token.jl
+++ src/token.jl
@@ -8,3 +8,18 @@ struct TokenAnnotation
 	node2
 	node2relation
 end
+
+
+"""Parse delimited string `s` into a `TokenAnnotation`."""
+function token(s; delimiter = "|")
+	parts = split(s, delimiter)
+	TokenAnnotation(CtsUrn(parts[1]),
+		parts[2],
+		parts[3],
+		parts[4],
+		parts[5],
+		parts[6],
+		parts[7],
+		parts[8]
+	)
+end
--- src/verbalunit.jl
+++ src/verbalunit.jl
@@ -34,19 +34,3 @@ function verbalunit(s; delimiter = "|")
 	)
 end
 
-
-#=
-"""Parse delimited string `s` into a `TokenAnnotation`."""
-function token(s; delimiter = "|")
-	parts = split(s, delimiter)
-	TokenAnnotation(CtsUrn(parts[1]),
-		parts[2],
-		parts[3],
-		parts[4],
-		parts[5],
-		parts[6],
-		parts[7],
-		parts[8]
-	)
-end
-=#
