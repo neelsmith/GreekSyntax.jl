760ac243823ca32cbcf3d23952c5910190bb057a
Neel Smith <dnsmith.neel@gmail.com>
2023-01-01 14:01:48 -0500

    0.3.0 release

--- src/mermaidutils.jl
+++ src/mermaidutils.jl
@@ -11,35 +11,38 @@ function mermaiddiagram(sa::SentenceAnnotation, tknannotations::Vector{TokenAnno
     zero = origin - 1
 
     impliedids = []
-    tknidx = 0
+    tknidx = zero
     for t in lextokens
         tknidx = tknidx + 1
 
         if isnothing(t.node1relation) || isnothing(t.node1)
             # skip
+            		
+        elseif t.node1 == "0" # asyndeton
+            push!(graphlines, string(tknidx, "[", t.text, "]", " --> |", t.node1relation, "|0[asyndeton];"))
+
         else
             n1int = parse(Int, t.node1) 
             if n1int > length(lextokens) && ! in(n1int, impliedids)
-                # PUSH AN IMPLIED LINE
-                push!(graphlines, string(tknidx, "[", t.text, "]", " --> |", t.node1relation, "| ", t.node1, "[implied];"))
+                push!(graphlines, string(tknidx, "[", t.text, "]", " --> |", t.node1relation, "| ", n1int + zero, "[implied];"))
             else
-                push!(graphlines, string(tknidx, "[", t.text, "]", " --> |", t.node1relation, "| ", t.node1 , "[", lextokens[n1int].text,  "];"))
+                push!(graphlines, string(tknidx, "[", t.text, "]", " --> |", t.node1relation, "| ", n1int + zero, "[", lextokens[n1int].text,  "];"))
             end
             
-
-			# HANDLE ASYNDETON:
-			#if ! isnothing(asynidx) && string(asynidx) == r.node1
-			#	push!(graphlines, "class $(tknidx) implicit;")
-			# end
 
 			if  isnothing(t.node2) || isempty(t.node2)
                  # skip
 			else
                 
                 n2int = parse(Int, t.node2)
-                push!(graphlines, string(tknidx, "[", t.text, "]", " --> |", t.node2relation, "| ", t.node1, "[", lextokens[n2int].text,  "];"))
+                if n2int > length(lextokens) && ! in(n1int, impliedids)
+                    # PUSH AN IMPLIED LINE
+                    push!(graphlines, string(tknidx, "[", t.text, "]", " --> |", t.node2relation, "| ", n2int + zero, "[implied];"))
+                else
+                    push!(graphlines, string(tknidx, "[", t.text, "]", " --> |", t.node2relation, "| ",  n2int + zero, "[", lextokens[n2int].text,  "];"))
+                end
 			end
         end
     end
-    graphstr = join(graphlines, "\n")
+    join(graphlines, "\n")
 end
