b1d92f45bccf78b81e764d2af48d00dbef0c4a3c
Neel Smith <dnsmith.neel@gmail.com>
2023-01-01 12:09:55 -0500

    Work on mermaid graphi ndexing

--- src/mermaidutils.jl
+++ src/mermaidutils.jl
@@ -8,15 +8,24 @@ function mermaiddiagram(sa::SentenceAnnotation, tknannotations::Vector{TokenAnno
     lextokens = filter(sentencetokens) do t
         t.tokentype == "lexical"
     end
-    tknidx = origin - 1
+    zero = origin - 1
+
+    impliedids = []
+    tknidx = 0
     for t in lextokens
         tknidx = tknidx + 1
 
         if isnothing(t.node1relation) || isnothing(t.node1)
             # skip
         else
-            n1int = parse(Int, t.node1)
-            push!(graphlines, string(tknidx, "[", t.text, "]", " --> |", t.node1relation, "| ", t.node1, "[", lextokens[n1int].text,  "];"))
+            n1int = parse(Int, t.node1) 
+            if n1int > length(lextokens) && ! in(n1int, impliedids)
+                # PUSH AN IMPLIED LINE
+                push!(graphlines, string(tknidx, "[", t.text, "]", " --> |", t.node1relation, "| ", t.node1, "[implied];"))
+            else
+                push!(graphlines, string(tknidx, "[", t.text, "]", " --> |", t.node1relation, "| ", t.node1 , "[", lextokens[n1int].text,  "];"))
+            end
+            
 
 			# HANDLE ASYNDETON:
 			#if ! isnothing(asynidx) && string(asynidx) == r.node1
@@ -26,6 +35,7 @@ function mermaiddiagram(sa::SentenceAnnotation, tknannotations::Vector{TokenAnno
 			if  isnothing(t.node2) || isempty(t.node2)
                  # skip
 			else
+                
                 n2int = parse(Int, t.node2)
                 push!(graphlines, string(tknidx, "[", t.text, "]", " --> |", t.node2relation, "| ", t.node1, "[", lextokens[n2int].text,  "];"))
 			end
