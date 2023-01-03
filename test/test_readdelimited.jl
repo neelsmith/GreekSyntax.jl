00133adf66980b405749050cb2810d32cb038ed3
Neel Smith <dnsmith.neel@gmail.com>
2023-01-03 14:06:03 -0500

    Add serialization of annotated tokens

--- test/test_readdelimited.jl
+++ test/test_readdelimited.jl
@@ -11,6 +11,15 @@ end
 
 
 @testset "Test reading and writing annotations" begin
+    sentdelim = "urn:cts:greekLit:tlg0540.tlg001.omar_tokens:1.8.1-1.8.15a|15|urn:cts:greekLit:tlg0540.tlg001.omar_tokens:1.8.3"
+    @test  delimited(GreekSyntax.sentence(sentdelim)) == sentdelim
+
     vudelim = "1.8.1-1.8.15a.1|independent clause|transitive|1|urn:cts:greekLit:tlg0540.tlg001.omar_tokens:1.8.1-1.8.15a"
     @test delimited(GreekSyntax.verbalunit(vudelim )) == vudelim
+
+    tkndelim  = "urn:cts:greekLit:tlg0540.tlg001.omar_tokens:1.7.29|lexical|γεγένηται|1.7.17-1.7.29a.2|2|unit verb|nothing|nothing"
+    @test delimited(GreekSyntax.token(tkndelim)) == tkndelim
+ 
 end
+
+
