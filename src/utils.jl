8f677d9de65bad959f86f04e7b02f88d0fc8fd9e
Neel Smith <dnsmith.neel@gmail.com>
2023-01-05 02:32:27 -0500

    Test data seleection utilities

--- src/utils.jl
+++ src/utils.jl
@@ -9,7 +9,6 @@ function sentencerange(orthotokens)
 	addpassage(baseurn, string(opener, "-", closer))
 end
 
-
 """Find CTS URN for range of tokens in `tokenlist`
 where `tokenlist` is a vector of annotated tokens.
 """
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using CitableCorpus, CitableBase, CitableText
 using Orthography, PolytonicGreek
 using Test
 
-
+include("test_utils.jl")
 include("test_selectionutils.jl")
 include("test_sentences.jl")
 include("test_readdelimited.jl")
