836cf2840db90f2a025476c53547287b08de6819
Neel Smith <dnsmith.neel@gmail.com>
2023-01-07 14:06:02 -0500

    Add placeholder for Union type on token URNs

--- src/token.jl
+++ src/token.jl
@@ -1,5 +1,5 @@
 struct TokenAnnotation
-	urn::CtsUrn
+	urn::Union{CtsUrn, Nothing}
 	tokentype
 	text
 	verbalunit
@@ -36,12 +36,15 @@ end
 function token(s; delimiter = "|")
 	parts = split(s, delimiter)
 
+	
+	urn = isnothing(parts[1]) ? nothing : CtsUrn(parts[1])
 	# 5-8
 	node1 = parts[5] == "nothing" ? nothing : parts[5]
 	node1rel = parts[6] == "nothing" ? nothing : parts[6]
 	node2 = parts[7] == "nothing" ? nothing : parts[7]
 	node2rel = parts[8] == "nothing" ? nothing : parts[8]
-	TokenAnnotation(CtsUrn(parts[1]),
+	TokenAnnotation(
+		urn,
 		parts[2],
 		parts[3],
 		parts[4],
